620ba5a00483d106a1ea8a1e5695959f63766e06
smestern <smestern@uwo.ca>
2019-09-07 22:49:02 -0400

    Added initial isolation function

---
+++ pyABF-APisolation/abfderivative.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pyabf
+from pyabf import filter
+import tkinter as tk
+from tkinter import filedialog
+import random
+
+
+
+
+def derivative(abf, sweepNumber, filter=0):
+    abf.setSweep(sweepNumber)
+    pyabf.filter.gaussian(abf,filter,0)
+    
+    dy=np.diff(abf.sweepY,1)
+    dx=np.diff(abf.sweepX,1)
+    dataY=dy/dx
+    dataX = 0.5*(abf.sweepX[:-1]+abf.sweepX[1:])
+
+
+    return dataX, dataY
+    
+def doublederivative(abf, sweepNumber, filter=0):
+    dataY = []
+    i2 = abf.sweepPointCount
+    pyabf.filter.gaussian(abf,filter,0)
+    abf.setSweep(sweepNumber)
+    dy=np.diff(abf.sweepY,1)
+    dx=np.diff(abf.sweepX,1)
+    dataY=dy/dx
+    dataX = 0.5*(abf.sweepX[:-1]+abf.sweepX[1:])
+    dyfirst=np.diff(dataY,1)
+    dxfirst=np.diff(dataX,1)
+    ysecond=dyfirst/dxfirst
+
+    xsecond=0.5*(dataX[:-1]+dataX[1:])
+    dataY = ysecond
+    dataX = xsecond
+    return dataX, dataY
+
+
+def integrate(abf, sweepNumber, filter=0, step=1, xlwllim=0, xupplim=-1):
+   i1, i2 = int(((xlwllim * 1000) * abf.dataPointsPerMs)), int(((xupplim * 1000) * abf.dataPointsPerMs))
+   pyabf.filter.gaussian(abf, filter, 0)
+   abf.setSweep(sweepNumber)
+   x = abf.sweepX[i1:i2]
+   y = abf.sweepY[i1:i2]
+   
+   area = np.trapz(y,x,step)
+
+   return area
+
+def derivintegrate(abf, sweepNumber, filter=0, step=1):
+   dataY2, dataX2 = [],[]
+   abf.setSweep(sweepNumber)
+   pyabf.filter.gaussian(abf,filter,0)
+   i1,i2 = 0,1
+   lstnum = 0
+   dy=np.diff(abf.sweepY,1)
+   dx=np.diff(abf.sweepX,1)
+   dataY=dy/dx
+   dataX = 0.5*(abf.sweepX[:-1]+abf.sweepX[1:])
+
+   for i in range(dataY.size):
+       i2 = i
+       i1 = lstnum
+       x = abf.sweepX[i1:i2]
+       y = abf.sweepY[i1:i2+1]
+       dataY2.append(np.trapz(y, None, 2))
+       lstnum = i2
+   dataX2 = 0.5*(abf.sweepX[:-1]+abf.sweepX[1:])
+   
+   
+   return dataX2, dataY2
---
+++ pyABF-APisolation/nuactionpotential.py
@@ -0,0 +1,149 @@
+#!/usr/bin/evn python
+
+import numpy as np
+from numpy import genfromtxt
+import scipy.linalg
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from abfderivative import *
+import pyabf
+from pyabf import filter
+from matplotlib import cm
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+root = tk.Tk()
+root.withdraw()
+
+
+def apfeaturearray(abf):
+    
+    return 0
+
+
+def thresholdavg(abf, sweep, thresdvdt = 20):
+    abf.setSweep(sweep)
+    apend = 0
+    aploc = 0
+    slopex, slopey = derivative(abf,sweep,1)
+    thresholdavghold = np.empty((1))
+    np.nan_to_num(abf.sweepY, nan=-9999, copy=False)
+    np.nan_to_num(slopey, nan=0, copy=False)
+    indexhigher = np.nonzero(np.where(slopey > thresdvdt, 1, 0)) #Returns indices only where the slope is greater than the threshold
+    indexhigher = indexhigher[0] #flattens
+    for j in indexhigher: 
+        if j > aploc:
+            k = slopey[j]
+            apend = int(j + (abf.dataPointsPerMs * 10)) #searches in the next 10ms for the peak
+            aploc = np.argmax(abf.sweepY[j:apend]) + j
+            if abf.sweepY[aploc] > -30: #Rejects ap if absolute peak is less than -30mv
+                maxdvdt = np.amax(slopey[j:aploc])
+                thresholdavghold = np.append(thresholdavghold, maxdvdt)
+    thresholdavg = np.mean(thresholdavghold[1:]) 
+    return thresholdavg
+
+def appreprocess2(abf, tag = 'default', save = False, plot = False):
+    apf = False
+    apstrt = 0
+    apend = 0
+    fileno, void = tag.split('-')
+    sweepcount = 1
+    apcount = 0
+    aps = np.full((4000, 5000), np.nan)
+    aplochold = 0
+    if abf.sweepCount > 1:
+        sweepcount = (abf.sweepCount - 1)
+    for sweepNumber in range(sweepcount):
+        print(sweepNumber)
+        abf.setSweep(sweepNumber)
+        aploc = 0
+        thresholdV = np.amax(abf.sweepY)
+        thresholdsl = (thresholdavg(abf,sweepNumber) * 0.05)
+        print(thresholdsl)
+        apf = False
+        slopex, slopey = derivative(abf,sweepNumber,1)
+        np.nan_to_num(abf.sweepY, nan=-9999, copy=False)
+        np.nan_to_num(slopey, nan=0, copy=False)
+        indexhigher = np.nonzero(np.where(slopey > thresholdsl, 1, 0)) #Returns indices only where the slope is greater than the threshold
+        indexhigher = indexhigher[0] #flattens
+        print(indexhigher)
+        for i in indexhigher:
+               if i > (aploc + (abf.dataPointsPerMs * 2)):
+                    apstrt = (int(i - (abf.dataPointsPerMs * 5)))
+                    if apstrt < 0: 
+                        apstrt=0
+                    apend = int(i + (abf.dataPointsPerMs * 20)) #searches in the next 5ms for the peak
+                    aploc = np.argmax(abf.sweepY[apstrt:apend]) + apstrt
+                    if abf.sweepY[aploc] > -30: #Rejects ap if absolute peak is less than -30mv
+                        print(aploc)
+                        apstrt = (int(aploc - abf.dataRate * 0.010))
+                        thresholdslloc = (np.argmax(slopey[apstrt:aploc]) + apstrt)
+                        apstrt = (int(apstrt - abf.dataRate * 0.020))
+                        if apstrt < 0:
+                            apstrt = 0
+                        idx = (np.abs(slopey[apstrt:thresholdslloc] - thresholdsl)).argmin()
+                        if idx < 1: 
+                            print(abf.sweepY[idx])
+                            print(thresholdsl)
+                        apstrt = idx + apstrt
+                        apend = abs(int(aploc + abf.dataPointsPerMs * 20))
+                        print('threshold Hit')
+                        k,  = abf.sweepY.shape
+                        if apend > k:
+                            apend = int(k)
+                        apfull1 = abf.sweepY[apstrt:apend]
+                        points = apend - apstrt
+                        #plt.plot(np.linspace(0,points,points), apfull1)
+                        aps[apcount,:points] = apfull1
+                        apcount += 1
+    if apcount > 0:
+        aps = aps[1:apcount,:]
+        apsend = np.argwhere(np.invert(np.isnan(aps[:,:])))
+        apsend = np.amax(apsend[:,1])
+        aps = aps[:,:apsend]
+        if plot == True:
+            void, l = aps.shape
+            test = np.linspace(0, l, num=l,endpoint=False)
+            for o in range(5):
+                j = int(random.uniform(1, apcount - 1))
+                if abf.dataRate > 10000:
+                    test = test[:int(l/2)]
+                    #plt.plot(test, aps[j,::2])
+                else:
+                    plt.plot(test, aps[j,:])
+    
+        if save == True:
+            np.savetxt('output/' + tag + '.txt', aps, delimiter=",", fmt='%12.5f')
+    return aps, abf
+
+def apisolate(abf, threshold, filter, tag = 'default', save = False):
+    aps, thresholdavg, abf = appreprocess(abf,tag,save)
+    
+
+
+
+    return 0
+
+
+
+
+directory = 'Processed/'
+
+
+for filename in os.listdir(directory):
+    if filename.endswith(".abf"):
+        file_path = directory + filename
+        abf = pyabf.ABF(file_path)
+
+        #print (abf.sweepLabelY)
+        if abf.sweepLabelY != 'Clamp Current (pA)':
+            print(filename + ' import')
+            tag = file_path.split('/')
+            print(str(thresholdavg(abf,0)))
+            appreprocess2(abf, tag[(len(tag) - 1)], False, True)
+            
+
+plt.show()
+
+
