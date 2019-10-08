98f5b5e391dead6a7f327a85957bbf1c10560ab1
smestern <smestern@uwo.ca>
2019-10-07 20:32:57 -0400

    Reordered

---
+++ pyAPisolation/bin/pyABF_APisolation.py
@@ -0,0 +1,35 @@
+
+
+import numpy as np
+from numpy import genfromtxt
+import pyabf
+import tkinter as tk
+from tkinter import filedialog
+import os
+import pandas
+import pyAPisolation as apis
+root = tk.Tk()
+root.withdraw()
+files = filedialog.askopenfilenames(filetypes=(('ABF Files', '*.abf'),
+                                   ('All files', '*.*')),
+                                   title='Select Input File'
+                                   )
+fileList = root.tk.splitlist(files)
+
+for filename in fileList:
+    if filename.endswith(".abf"):
+        file_path = filename
+        abf = pyabf.ABF(file_path)
+        if abf.sweepLabelY != 'Clamp Current (pA)':
+            print(filename + ' import')
+            np.nan_to_num(abf.data, nan=-9999, copy=False)
+            tag = file_path.split('/')
+            tag = tag[(len(tag) - 1)]
+            #fileno, void = tag.split('-')
+            apis.nuactionpotential.thresholdavg(abf,0)
+            _, df, _ = apis.nuactionpotential.apisolate(abf, 0, "", False, True, plot=8)
+        else:
+            print('Not Current CLamp')
+                     
+
+plt.show()
