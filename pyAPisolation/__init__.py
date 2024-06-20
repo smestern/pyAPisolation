24153905bf95b4bc1f607ce73ce71a44b8a73d9c
smestern <smestern@gmail.com>
2024-06-20 11:25:55 -0400

    moving tests

--- pyAPisolation/__init__.py
+++ pyAPisolation/__init__.py
@@ -1,2 +1,18 @@
 name = 'pyAPisolation'
-from . import dev
+version = '0.1.0'
+## we need to post install hook to install ipfx, since its a bit of a nightmare
+## to install.
+import os
+import sys
+import subprocess
+
+# install ipfx without deps
+def install_ipfx():
+    subprocess.run([sys.executable, "-m", "pip", "install", "ipfx", "--no-deps"])
+
+    
+#only call if ipfx is not installed
+try:
+    import ipfx
+except ImportError:
+    install_ipfx()
