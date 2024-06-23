f4d8e9a97212b6c4de10c38579f7f4eff33a8d2c
Sam Mestern <smestern@uwo.ca>
2024-06-23 16:42:52 -0400

    update tests

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,14 +1,19 @@
 import os
 import sys
+import subprocess
 import logging
 
 #create a conda env called pyAPisolation_test and install the package in it
 def test_install():
-    os.system("conda create -n pyAPisolation_test python=3.7")
-    os.system("conda activate pyAPisolation_test")
-    ret = os.system("pip install .[all]")
-    os.system("conda deactivate")
-    os.system("conda remove -n pyAPisolation_test --all")
+    p = subprocess.Popen('conda create -n pyAPisolation_test python=3.9 -y', shell=True)
+    p.wait()
+    p = subprocess.Popen('conda activate pyAPisolation_test && pip install -e .', shell=True)
+    p.wait()
+    p = subprocess.Popen('conda deactivate', shell=True)
+    p.wait()
+    p = subprocess.Popen('conda remove -n pyAPisolation_test --all -y', shell=True)
+    p.wait()
+
     assert True
 
 if __name__ == "__main__":
