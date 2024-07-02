729ea3f84bead3ce3ba1478ee8ffec8cc2cbdd43
smestern <smestern@gmail.com>
2024-07-02 11:01:30 -0400

    typo

--- pyAPisolation/webViz/ephysDatabase.py
+++ pyAPisolation/webViz/ephysDatabase.py
@@ -144,7 +144,7 @@ def main(database_file=None, config=None, static=False):
         #insert nan values for the outliers
         for idx in outlier_idx:
             umap_data = np.insert(umap_data, idx, np.nan, axis=0)
-        full_dataframe['Umap X'] = umap_data[:, 0]
+        full_dataframe['Umap X'] = ]umap_data[:, 0]
         full_dataframe['Umap Y'] = umap_data[:, 1]
         print("Umap columns generated")
     else:
